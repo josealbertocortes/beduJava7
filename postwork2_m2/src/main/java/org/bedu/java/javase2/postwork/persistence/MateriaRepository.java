574017ec5710db8df8cc427f1224b21a12bed70b
LuisMartinez <lamc44@hotmail.com>
2023-04-18 19:17:54 -0600

    Se agrega postwork 2 y 3 del modulo 2

---
+++ postwork2_m2/src/main/java/org/bedu/java/javase2/postwork/persistence/MateriaRepository.java
@@ -0,0 +1,7 @@
+package org.bedu.java.javase2.postwork.persistence;
+
+import org.bedu.java.javase2.postwork.model.Materia;
+import org.springframework.data.repository.CrudRepository;
+
+public interface MateriaRepository extends CrudRepository<Materia, Long> {
+}
---
+++ postwork3_m2/src/main/java/org/bedu/java/javase2/postwork/PostworkApplication.java
@@ -0,0 +1,13 @@
+package org.bedu.java.javase2.postwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostworkApplication.class, args);
+    }
+
+}
